78c0344623f5d12a93743d2523dfdb7e56be4624
Fábio Pereira da Silva <fabioegel@gmail.com>
2024-12-01 11:12:04 -0300

    Add numeric checker

--- i3ecnv.c
+++ i3ecnv.c
@@ -2,9 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 _Static_assert(sizeof(float)==sizeof(unsigned int), "Architecture error");
 _Static_assert(sizeof(double)==sizeof(unsigned long long int), "Architeture error double/long int");
+
+bool isNumeric(const char *value)
+{
+
+  if (value[0] == 0)
+    return false;
+
+  const char *list[] = {"inf", "-inf", "+inf", "nan", "-nan", "+nan", NULL}, **lp = list;
+
+  while (*lp)
+    if (strcasecmp(*(lp++), value) == 0)
+      return true;
+
+  char c = (char)*(value);
+
+  if (c == 'E' || c == 'e')
+    return false;
+
+  bool scientifcNotationOccur = false;
+  bool occur = false;
+
+part2:
+
+  if (c == '+' || c == '-')
+    if ((c = (char)*(++value)) == 0)
+      return false;
+
+  do {
+    if (c < '0') {
+      if (c == '.')
+        if (!occur) {
+          occur = true;
+          continue;
+        }
+
+      return false;
+    }
+
+    if (c > '9') {
+
+      if ((c != 'E') && (c != 'e'))
+        return false;
+
+      if (!scientifcNotationOccur)
+        if ((c = (char)*(++value))) {
+          scientifcNotationOccur = true;
+          goto part2;
+        }
+
+      return false;
+    }
+
+  } while ((c = (char)*(++value)));
+
+  return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -28,6 +85,12 @@ int main(int argc, char **argv)
     }
 
     num_arg=(const char *)argv[1];
+
+    if (!isNumeric(num_arg)) {
+      printf("\n%s is not a number\n", num_arg);
+      return -2;
+    }
+
     memset(&value, 0, VALUE_U_SZ);
 
     value.d=strtod(num_arg, NULL);
@@ -42,3 +105,4 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
